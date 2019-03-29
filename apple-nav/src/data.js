a40a312cd4ff5dbdc59c5186b8087374e360bf14
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-29 10:16:35 -0700

    Changed some icons in SabNav, added netlify.toml for continuous deployment settings

--- apple-nav/src/data.js
+++ apple-nav/src/data.js
@@ -7,52 +7,52 @@ const data = [
             {
                 id: 1.01,
                 title: 'MacBook',
-                img: 'https://img.icons8.com/ios/50/000000/macbook.png'
+                img: 'https://img.icons8.com/ios/100/000000/macbook.png'
             },
             {
                 id: 1.02,
                 title: 'MacBook Air',
-                img: 'https://img.icons8.com/ios/50/000000/macbook.png'
+                img: 'https://img.icons8.com/ios/100/000000/macbook.png'
             },
             {
                 id: 1.03,
                 title: 'MacBook Pro',
-                img: 'https://img.icons8.com/ios/50/000000/macbook.png'
+                img: 'https://img.icons8.com/ios/100/000000/macbook.png'
             },
             {
                 id: 1.04,
                 title: 'iMac',
-                img: 'https://img.icons8.com/ios/50/000000/imac.png'
+                img: 'https://img.icons8.com/ios/100/000000/imac.png'
             },
             {
                 id: 1.05,
                 title: 'iMac Pro',
-                img: 'https://img.icons8.com/ios/50/000000/imac.png'
+                img: 'https://img.icons8.com/ios/100/000000/imac.png'
             },
             {
                 id: 1.06,
                 title: 'Mac Pro',
-                img: 'https://img.icons8.com/ios/50/000000/steve-jobs.png'
+                img: 'https://img.icons8.com/ios/100/000000/steve-jobs.png'
             },
             {
                 id: 1.07,
                 title: 'Mac mini',
-                img: 'https://img.icons8.com/ios/50/000000/steve-jobs.png'
+                img: 'https://img.icons8.com/ios/100/000000/steve-jobs.png'
             },
             {
                 id: 1.08,
                 title: 'Accessories',
-                img: 'https://img.icons8.com/ios/50/000000/headphones-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/headphones-filled.png'
             },
             {
                 id: 1.09,
                 title: 'Mojave',
-                img: 'https://img.icons8.com/ios/50/000000/mac-client.png'
+                img: 'https://img.icons8.com/ios/100/000000/mac-client.png'
             },
             {
                 id: 1.10,
                 title: 'Compare',
-                img: 'https://img.icons8.com/ios/50/000000/compare.png'
+                img: 'https://img.icons8.com/ios/100/000000/compare.png'
             }
         ]
     },
@@ -64,47 +64,47 @@ const data = [
             {
                 id: 2.01,
                 title: 'iPad Pro',
-                img: 'https://img.icons8.com/ios/50/000000/ipad.png'
+                img: 'https://img.icons8.com/ios/100/000000/ipad.png'
             },
             {
                 id: 2.02,
                 title: 'iPad Air',
-                img: 'https://img.icons8.com/ios/50/000000/ipad.png'
+                img: 'https://img.icons8.com/ios/100/000000/ipad.png'
             },
             {
                 id: 2.03,
                 title: 'iPad',
-                img: 'https://img.icons8.com/ios/50/000000/ipad.png'
+                img: 'https://img.icons8.com/ios/100/000000/ipad.png'
             },
             {
                 id: 2.04,
                 title: 'iPad mini',
-                img: 'https://img.icons8.com/ios/50/000000/ipad-mini.png'
+                img: 'https://img.icons8.com/ios/100/000000/ipad-mini.png'
             },
             {
                 id: 2.05,
                 title: 'Compare',
-                img: 'https://img.icons8.com/ios/50/000000/smartphone-tablet.png'
+                img: 'https://img.icons8.com/ios/100/000000/smartphone-tablet.png'
             },
             {
                 id: 2.06,
                 title: 'Apple Pencil',
-                img: 'https://img.icons8.com/ios/50/000000/edit-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/edit-filled.png'
             },
             {
                 id: 2.07,
                 title: 'Smart Keyboard',
-                img: 'https://img.icons8.com/ios/50/000000/keyboard.png'
+                img: 'https://img.icons8.com/ios/100/000000/keyboard.png'
             },
             {
                 id: 2.08,
                 title: 'Accessories',
-                img: 'https://img.icons8.com/ios/50/000000/headphones-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/headphones-filled.png'
             },
             {
                 id: 2.09,
                 title: 'iOS 12',
-                img: 'https://img.icons8.com/ios/50/000000/ios-logo.png'
+                img: 'https://img.icons8.com/ios/100/000000/ios-logo.png'
             }
         ]
     },
@@ -116,42 +116,42 @@ const data = [
             {
                 id: 3.01,
                 title: 'iPhone Xs',
-                img: 'https://img.icons8.com/ios/50/000000/iphone-x-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/iphone-x-filled.png'
             },
             {
                 id: 3.02,
                 title: 'iPhone Xr',
-                img: 'https://img.icons8.com/ios/50/000000/iphone-x-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/iphone-x-filled.png'
             },
             {
                 id: 3.03,
                 title: 'iPhone 8',
-                img: 'https://img.icons8.com/ios/50/000000/iphone.png'
+                img: 'https://img.icons8.com/ios/100/000000/iphone.png'
             },
             {
                 id: 3.04,
                 title: 'iPhone 7',
-                img: 'https://img.icons8.com/ios/50/000000/iphone.png'
+                img: 'https://img.icons8.com/ios/100/000000/iphone.png'
             },
             {
                 id: 3.05,
                 title: 'iOS 12',
-                img: 'https://img.icons8.com/ios/50/000000/ios-logo.png'
+                img: 'https://img.icons8.com/ios/100/000000/ios-logo.png'
             },
             {
                 id: 3.06,
                 title: 'AirPods',
-                img: 'https://img.icons8.com/ios/50/000000/earbud-headphones.png'
+                img: 'https://img.icons8.com/ios/100/000000/earbud-headphones.png'
             },
             {
                 id: 3.07,
                 title: 'Accessories',
-                img: 'https://img.icons8.com/ios/50/000000/headphones-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/headphones-filled.png'
             },
             {
                 id: 3.08,
                 title: 'Compare',
-                img: 'https://img.icons8.com/ios/50/000000/smartphone-tablet.png'
+                img: 'https://img.icons8.com/ios/100/000000/smartphone-tablet.png'
             }
         ]
     },
@@ -163,42 +163,42 @@ const data = [
             {
                 id: 4.01,
                 title: 'Apple Watch Series 4',
-                img: 'https://img.icons8.com/ios/50/000000/apple-watch-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/apple-watch-filled.png'
             },
             {
                 id: 4.02,
                 title: 'Apple Watch Nike+',
-                img: 'https://img.icons8.com/ios/50/000000/watches-side-view.png'
+                img: 'https://img.icons8.com/ios/100/000000/watches-side-view.png'
             },
             {
                 id: 4.03,
                 title: 'Apple Watch Hermès',
-                img: 'https://img.icons8.com/ios/50/000000/watches-side-view-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/watches-side-view-filled.png'
             },
             {
                 id: 4.04,
                 title: 'Apple Watch Series 3',
-                img: 'https://img.icons8.com/ios/50/000000/apple-watch.png'
+                img: 'https://img.icons8.com/ios/100/000000/apple-watch.png'
             },
             {
                 id: 4.05,
                 title: 'watchOS',
-                img: 'https://img.icons8.com/ios/50/000000/operating-system.png'
+                img: 'https://img.icons8.com/ios/100/000000/operating-system.png'
             },
             {
                 id: 4.06,
                 title: 'Bands',
-                img: 'https://img.icons8.com/ios/50/000000/watches-clasp.png'
+                img: 'https://img.icons8.com/ios/100/000000/watches-clasp.png'
             },
             {
                 id: 4.07,
                 title: 'Accessories',
-                img: 'https://img.icons8.com/ios/50/000000/headphones-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/headphones-filled.png'
             },
             {
                 id: 4.08,
                 title: 'Compare',
-                img: 'https://img.icons8.com/ios/50/000000/compare.png'
+                img: 'https://img.icons8.com/ios/100/000000/compare.png'
             }
         ]
     },
@@ -210,32 +210,32 @@ const data = [
             {
                 id: 5.01,
                 title: 'Apple TV app',
-                img: 'https://img.icons8.com/ios/50/000000/apple-tv.png'
+                img: 'https://img.icons8.com/ios/100/000000/apple-tv.png'
             },
             {
                 id: 5.02,
                 title: 'Apple TV+',
-                img: 'https://img.icons8.com/ios/50/000000/retro-tv.png'
+                img: 'https://img.icons8.com/ios/100/000000/retro-tv.png'
             },
             {
                 id: 5.03,
                 title: 'Apple TV 4K',
-                img: 'https://img.icons8.com/ios/50/000000/4k.png'
+                img: 'https://img.icons8.com/ios/100/000000/4k.png'
             },
             {
                 id: 5.04,
                 title: 'Apple TV HD',
-                img: 'https://img.icons8.com/ios/50/000000/hdtv.png'
+                img: 'https://img.icons8.com/ios/100/000000/hdtv.png'
             },
             {
                 id: 5.05,
                 title: 'AirPlay',
-                img: 'https://img.icons8.com/ios/50/000000/air-play.png'
+                img: 'https://img.icons8.com/ios/100/000000/air-play.png'
             },
             {
                 id: 5.06,
                 title: 'Accessories',
-                img: 'https://img.icons8.com/ios/50/000000/headphones-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/headphones-filled.png'
             }
         ]
     },
@@ -247,37 +247,37 @@ const data = [
             {
                 id: 6.01,
                 title: 'Apple Music',
-                img: 'https://img.icons8.com/ios/50/000000/itunes-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/itunes-filled.png'
             },
             {
                 id: 6.02,
                 title: 'iTunes',
-                img: 'https://img.icons8.com/ios/50/000000/christmas-star-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/christmas-star-filled.png'
             },
             {
                 id: 6.03,
                 title: 'HomePod',
-                img: 'https://img.icons8.com/ios-glyphs/50/000000/apple-homepod.png'
+                img: 'https://img.icons8.com/ios-glyphs/100/000000/apple-homepod.png'
             },
             {
                 id: 6.04,
                 title: 'AirPods',
-                img: 'https://img.icons8.com/ios/50/000000/earbud-headphones.png'
+                img: 'https://img.icons8.com/ios/100/000000/earbud-headphones.png'
             },
             {
                 id: 6.05,
                 title: 'iPod touch',
-                img: 'https://img.icons8.com/ios/50/000000/ipod-old-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/ipod-old-filled.png'
             },
             {
                 id: 6.06,
                 title: 'Music Accessories',
-                img: 'https://img.icons8.com/ios/50/000000/headphones-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/headphones-filled.png'
             },
             {
                 id: 6.07,
                 title: 'Gift Cards',
-                img: 'https://img.icons8.com/ios/50/000000/apple-pay-filled.png'
+                img: 'https://img.icons8.com/ios/100/000000/apple-pay-filled.png'
             }
         ]
     },
